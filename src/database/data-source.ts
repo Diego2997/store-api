6b1ac15118bab81efff30d93543ec379cc795df8
Diego Mercado <diego.mercado1997@hotmail.com>
2024-02-02 19:28:23 -0300

    se instala dotenv para utilizar las variables de entorno en la configuracion del data-source

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,12 +1,15 @@
+import * as dotenv from 'dotenv';
 import { DataSource } from 'typeorm';
+
+dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: 'localhost',
-  database: 'my_db',
-  username: 'root',
-  password: '123456',
-  port: 5432,
+  database: process.env.POSTGRES_DB,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  port: +process.env.POSTGRES_PORT,
   migrationsTableName: 'migrations',
   synchronize: false,
   entities: ['src/**/**/*.entity.ts'],
