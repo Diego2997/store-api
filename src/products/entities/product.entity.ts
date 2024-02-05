1fbc405b869bccd2bb40c939425f3ffd5f4fff19
Diego Mercado <diego.mercado1997@hotmail.com>
2024-02-05 10:37:21 -0300

    se realiza la relacion muchos a muchos y para eso se utilizo los decoradores ManyToMany y JoinTable

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,6 +1,7 @@
 import { Brand } from 'src/brands/entities/brand.entity';
 import { BaseEntity } from 'src/common/entity/base.entity';
-import { Column, Entity, ManyToOne } from 'typeorm';
+import { Column, Entity, JoinTable, ManyToMany, ManyToOne } from 'typeorm';
+import { Category } from './category.entity';
 
 @Entity()
 export class Product extends BaseEntity {
@@ -21,4 +22,8 @@ export class Product extends BaseEntity {
 
   @ManyToOne(() => Brand, (brand) => brand.products)
   brand: Brand;
+
+  @ManyToMany(() => Category, (category) => category.products)
+  @JoinTable()
+  categories: Category[];
 }
